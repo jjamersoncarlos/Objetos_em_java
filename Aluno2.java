283ccd8e65140d5ebe3356b44d688a9802f3b615
Jamerson <jjamersoncarlos.s@gmail.com>
2026-05-30 23:34:12 -0300

    exercicio 7 e 8

---
+++ Aluno2.java
@@ -0,0 +1,21 @@
+public class Aluno2 {
+
+    String nome;
+    double nota1;
+    double nota2;
+
+    public double calcularMedia() {
+        return (nota1 + nota2) / 2;
+    }
+
+    public String verificarSituacao() {
+        double media = calcularMedia();
+
+        if (media >= 7) {
+            return "Aprovado";
+        } else {
+            return "Reprovado";
+        }
+    }
+    
+}
---
+++ Mainaluno2.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Mainaluno2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int aprovados = 0;
+        int reprovados = 0;
+
+        System.out.print("Quantos alunos deseja cadastrar? ");
+        int quantidade = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 1; i <= quantidade; i++) {
+            Aluno aluno = new Aluno();
+
+            System.out.println("\nCadastro do aluno " + i);
+
+            System.out.print("Nome: ");
+            aluno.nome = scanner.nextLine();
+
+            System.out.print("Nota 1: ");
+            aluno.nota1 = scanner.nextDouble();
+
+            System.out.print("Nota 2: ");
+            aluno.nota2 = scanner.nextDouble();
+            scanner.nextLine();
+
+            System.out.println("Média de " + aluno.nome + ": " + aluno.calcularMedia());
+            System.out.println("Situação: " + aluno.verificarSituacao());
+
+            if (aluno.calcularMedia() >= 7) {
+                aprovados = aprovados + 1;
+            } else {
+                reprovados = reprovados + 1;
+            }
+        }
+
+        System.out.println("\n===== RESULTADO FINAL =====");
+        System.out.println("Quantidade de aprovados: " + aprovados);
+        System.out.println("Quantidade de reprovados: " + reprovados);
+
+        scanner.close();
+    }
+}
